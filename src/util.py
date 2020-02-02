61d50057b364ab205ddcab78e709f7c1c88d8797
John Hsu <john.r.n.hsu@gmail.com>
2020-02-02 10:02:13 -0500

    Added setFullHeight

---
+++ src/util.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+
+This file holds the utility functions for EasyLeague
+
+Author: John Hsu and Sergey Satskiy
+
+"""
+
+
+def setFullHeight(treeWidget):
+    """
+    Sets the full height for a QTreeWidget object to display all rows.
+    """
+    fullHeight = 0
+    topLevelCount = treeWidget.topLevelItemCount()
+
+    for rowIndex in range(topLevelCount):
+        item = treeWidget.topLevelItem(rowIndex)
+        fullHeight += treeWidget.rowHeight(treeWidget.indexFromItem(item))
+
+    if fullHeight != 0:
+        fullHeight += treeWidget.header().sizeHint().height()
+        treeWidget.setMinimumHeight(fullHeight)
